7aab4e594c8d81bb0c264e679144a7fd900a46de
Guillaume Attia <guillaume.attia91@gmail.com>
2023-11-25 15:04:33 +0100

    feat: download youtube video

---
+++ src/commands.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+import click
+
+from repertoir.download_video import download_youtube_video
+from repertoir.search_video_repertoir import search_video_repertoire
+
+
+@click.group()
+def cli():
+    pass
+
+
+@cli.command("download")
+@click.argument("url")
+def cmd_download_youtube_video(url: str):
+    download_youtube_video(url)
+
+
+@cli.command("search-video")
+def cmd_search_video():
+    search_video_repertoire()
+
+
+if __name__ == "__main__":
+    cli()
